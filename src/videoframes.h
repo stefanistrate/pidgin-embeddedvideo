27c14a6b8308185a02891b4a625ab8669dceeeef
stefan.istrate <stefan.istrate@7caa73a2-9fcf-11de-b29f-73f7923e6196>
2009-09-12 19:44:07 +0000

    Moving project from pidgin-video.

---
+++ src/videoframes.h
@@ -0,0 +1,34 @@
+#ifndef VIDEOFRAMES_H
+
+#define VIDEOFRAMES_H
+
+#include <glib.h>
+#include <gtk/gtk.h>
+
+#include <gtkimhtml.h>
+
+#include "websites.h"
+
+typedef struct _ButtonInfo ButtonInfo;
+
+struct _ButtonInfo
+{
+    GtkIMHtml   *imhtml;
+    GtkTextMark *mark;
+    WebsiteInfo *website;
+    GString     *url;
+};
+
+ButtonInfo * button_info_new(GtkIMHtml *, GtkTextIter *,
+        WebsiteInfo *, gchar *, gint);
+void         button_info_free(ButtonInfo *);
+
+void         videoframes_init();
+void         videoframes_destroy();
+GtkWidget *  videoframes_insert_new_button(GtkIMHtml *, GtkTextIter *,
+        WebsiteInfo *, gchar *, gint);
+void         videoframes_remove_button(GtkWidget *);
+void         videoframes_toggle_button(GtkWidget *);
+gchar *      videoframes_generate_page(WebsiteInfo *, GString *);
+
+#endif
