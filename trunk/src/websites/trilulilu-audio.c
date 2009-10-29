3f412b28d01b960787389793e0bbdfe0432f1cd2
Laurentiu.Stroe@gmail.com <Laurentiu.Stroe@gmail.com@7caa73a2-9fcf-11de-b29f-73f7923e6196>
2009-10-29 16:36:52 +0000

    Get the HTML page with curl

--- trunk/src/websites/trilulilu-audio.c
+++ trunk/src/websites/trilulilu-audio.c
@@ -6,10 +6,10 @@
 #include <debug.h>
 #include <config.h>
 
-static gchar buffer[1 << 20];
-static size_t buflen = 1 << 20;
+static GString *buffer;
 
 static int check(gchar *const);
+static size_t save_downloaded_content(gchar *, size_t, size_t, void *);
 
 WebsiteInfo trilulilu_audio = {
     "trilulilu-audio",
@@ -23,27 +23,32 @@ WebsiteInfo trilulilu_audio = {
     &check
 };
 
+size_t save_downloaded_content(gchar *t_buffer, size_t size, size_t nmemb, void *stream)
+{
+	g_string_append_len(buffer, t_buffer, nmemb);
+	return nmemb;
+}
+
 static int check(gchar *const link)
 {
     CURL *curl;
-    CURLcode res;
-    size_t n;
-
+    CURLcode curl_res;
+	
+	buffer = g_string_sized_new(1 << 18);
+	
     curl = curl_easy_init();
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, link);
-        /* FIXME: Get the HTML page with curl. */
-        res = curl_easy_recv(curl, buffer, buflen, &n);
-
-        if (res == CURLE_OK) {
-            buffer[n] = 0;
-            if (g_regex_match_simple("var\\ current_file\\ =\\ \\{[^\\}]*\"mimetype\":\"audio\"[^\\}]*\\}",
-                        buffer, 0, 0))
-                return 1;
-        }
-
-        curl_easy_cleanup(curl);
+	    curl_easy_setopt(curl, CURLOPT_URL, link);
+	    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_downloaded_content);
+	    curl_res = curl_easy_perform(curl);
+	    
+		curl_easy_cleanup(curl);
+	
+	    if (curl_res == CURLE_OK) {	
+    		if (g_regex_match_simple("var\\ current_file\\ =\\ \\{.*\"mimetype\":\"audio\".*\\}",
+    	 	  	        buffer->str, 0, 0))
+		    	return 1;
+		}
     }
-
     return 0;
 }
