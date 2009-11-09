1275ca1893a148e2ebfd308c8a477e1101312d8b
stefan.istrate <stefan.istrate@7caa73a2-9fcf-11de-b29f-73f7923e6196>
2009-11-09 01:50:53 +0000

    Added support for CollegeHumor and Dailymotion.

---
+++ trunk/src/websites/collegehumor.c
@@ -0,0 +1,14 @@
+#include <websites.h>
+
+WebsiteInfo collegehumor = {
+    "collegehumor",
+    "^(?i)(?<http>http://)?(?(<http>)(www\\.)?|(www\\.))collegehumor\\.com/video:(?<video_id>\\d+)(\\D.*)?$",
+    "<object type=\"application/x-shockwave-flash\" data=\"http://www.collegehumor.com/moogaloop/moogaloop.swf?clip_id=%VIDEO_ID%&fullscreen=1\" width=\"425\" height=\"320\" >"
+        "<param name=\"allowfullscreen\" value=\"true\"/>"
+        "<param name=\"wmode\" value=\"transparent\"/>"
+        "<param name=\"allowScriptAccess\" value=\"always\"/>"
+        "<param name=\"movie\" quality=\"best\" value=\"http://www.collegehumor.com/moogaloop/moogaloop.swf?clip_id=%VIDEO_ID%&fullscreen=1\"/>"
+        "<embed src=\"http://www.collegehumor.com/moogaloop/moogaloop.swf?clip_id=%VIDEO_ID%&fullscreen=1\" type=\"application/x-shockwave-flash\" wmode=\"transparent\"  width=\"425\" height=\"320\"  allowScriptAccess=\"always\"></embed>"
+        "</object>",
+    NULL
+};
