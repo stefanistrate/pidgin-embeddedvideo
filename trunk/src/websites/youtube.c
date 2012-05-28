f40458f20d2c08872e5820143bdfca9bbb8c1188
stefan.istrate@gmail.com <stefan.istrate@gmail.com@7caa73a2-9fcf-11de-b29f-73f7923e6196>
2012-05-28 23:43:03 +0000

    Relaxed the regex and updated the embedding code for Youtube

--- trunk/src/websites/youtube.c
+++ trunk/src/websites/youtube.c
@@ -2,13 +2,7 @@
 
 WebsiteInfo youtube = {
     "youtube",
-    "^(?i)(?<http>http://)?(?(<http>)(www\\.)?|(www\\.))youtube\\.com/watch\\?(?<q1>(?<query>(([a-uw-z\\d-][\\w\\d-]*)|(v[\\w\\d-]+))(=[\\w\\d-~+%\\.]*)?|(=[\\w\\d-~+%\\.]*)))?(?<q2>[&;](?&query)?)*(?(<q1>)[&;]|(?(<q2>)[&;]|[&;]?))v=(?-i)(?<video_id>[\\w\\d-]{11})(?i)([&;](?&query)?)*(#[\\w\\d-=]*)?$",
-    "<object width=\"425\" height=\"344\">"
-        "<param name=\"movie\" value=\"http://www.youtube.com/v/%VIDEO_ID%&fs=1&rel=0\"></param>"
-        "<param name=\"allowFullScreen\" value=\"true\"></param>"
-        "<param name=\"allowscriptaccess\" value=\"always\"></param>"
-        "<param name=\"wmode\" value=\"opaque\"></param>"
-        "<embed src=\"http://www.youtube.com/v/%VIDEO_ID%&fs=1&rel=0\" type=\"application/x-shockwave-flash\" allowscriptaccess=\"always\" allowfullscreen=\"true\" wmode=\"opaque\" width=\"425\" height=\"344\"></embed>"
-        "</object>",
+    "^(?i)(https?://)?(\\w+\\.)?youtube\\.com/watch\\?(.*&)?v=(?-i)(?<video_id>[\\w\\d-]{11})(?i)([^\\w\\d-].*)?$",
+    "<iframe width=\"425\" height=\"240\" src=\"http://www.youtube.com/embed/%VIDEO_ID%\" frameborder=\"0\" allowfullscreen></iframe>",
     NULL
 };
