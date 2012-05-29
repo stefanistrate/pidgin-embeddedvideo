467bdf73d3688a9a4e78b6aedd08fd94c326af7c
stefan.istrate@gmail.com <stefan.istrate@gmail.com@7caa73a2-9fcf-11de-b29f-73f7923e6196>
2012-05-29 00:22:25 +0000

    Removed support for Xfire Updated ChangeLog

--- src/websites/xfire.c
+++
@@ -1,8 +0,0 @@
-#include <websites.h>
-
-WebsiteInfo xfire = {
-    "xfire",
-    "^(?i)(https?://)?(\\w+\\.)?xfire\\.com/videos/(?<video_id>[\\w\\d]+)([^\\w\\d].*)?$",
-    "<iframe src=\"http://beta.xfire.com/videos/%VIDEO_ID%/embed\" width=\"425\" height=\"266\" frameborder=\"0\"></iframe>",
-    NULL
-};
